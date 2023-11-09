a569aa2c7a352098532150e6f94a4759687e0f39
SU <valej200554@gmail.com>
2023-11-09 11:56:41 +0500

    add

--- testing/client/src/pages/home/Home.tsx
+++ testing/client/src/pages/home/Home.tsx
@@ -1,20 +1,21 @@
 // import { Routes, Route, Link } from "react-router-dom";
 // import styles from './Home.module.css'
-import { useState } from "react";
-import HeaderIntro from "./components/intro/HeaderIntro";
-import Modal from "./components/modal/Modal";
+import { useState } from "react"
+import HeaderIntro from "./components/intro/HeaderIntro"
+import Modal from "./components/modal/Modal"
+import IntroMain from "./components/introMain/IntroMain"
 
 interface IHomeProps {
   modalAuthActive: boolean
   setModalAuthActive: (value: boolean) => void
 }
 
-function Home({modalAuthActive, setModalAuthActive}:IHomeProps) {
-    
+function Home({ modalAuthActive, setModalAuthActive }: IHomeProps) {
   return (
     <>
-      <HeaderIntro/>
+      <HeaderIntro />
       <Modal active={modalAuthActive} setActive={setModalAuthActive} />
+      <IntroMain />
     </>
   )
 }
