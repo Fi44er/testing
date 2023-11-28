0e84a6198504cd9c2d7c5fc46c180587311b0b00
SU <valej200554@gmail.com>
2023-11-28 15:26:15 +0500

    add

--- testing/client/src/pages/profile/quiz/components/Quiz.tsx
+++ testing/client/src/pages/profile/quiz/components/Quiz.tsx
@@ -1,4 +1,7 @@
-import style from "./Quiz.module.css";
+import React from "react"
+
+import style from "./Quiz.module.css"
+import { useState } from "react"
 
 const Data: any = [
   {
@@ -127,7 +130,7 @@ const Data: any = [
     answer2: "Художественная форма, мастерство писателя или режиссера",
     answer3: "Сюжет, действия героев",
   },
-];
+]
 
 // const quiz = document.getElementById("quiz");
 // const qustions = document.getElementById("qustions");
@@ -144,53 +147,129 @@ const Data: any = [
 
 //   );
 // });
-const DataContent = Data.map((item: any) => {
-  const correct1 = Data[0].correct;
-  console.log(Data[0][correct1]);
+// const DataContent = Data.map((item: any) => {
+//   const correct1 = Data[0].correct;
+//   console.log(Data[0][correct1]);
+
+//   return (
+//     <div>
+//       <div className={style.quizQustions} id="qustions">
+//         <div className={style.quizQustionsItem}>
+//           <div className={style.quizQustionsItemQustion}>{item.qustion}</div>
+//           <ul className={style.quizQustionsItemAnswers}>
+//             <li>
+//               <label>
+//                 <input type="radio" name="q1" />
+//                 <div className={style.answer}>
+//                   <p>{item.answer1}</p>
+//                 </div>
+//               </label>
+//             </li>
+//             <li>
+//               <label>
+//                 <input type="radio" name="q1" />
+//                 <div className={style.answer}>
+//                   <p>{item.answer2}</p>
+//                 </div>
+//               </label>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//       {/* <div className={style.quizResults} id="results">
+//         <div className={style.quizResultsItem}>
+//           <div className={style.quizQustionsItemQustion}>{item.qustion}</div>
+//           {answersRender}
+//         </div>
+//       </div> */}
+//     </div>
+//   );
+// });
+
+enum Answer {
+  Like,
+  DisLike,
+  Idk,
+}
+
+const Quiz = () => {
+  const [data, setData] = useState<
+    {
+      question: string
+      answer?: Answer | undefined
+    }[]
+  >([
+    {
+      question: "В книге или кинофильме меня больше всего привлекает:",
+    },
+    {
+      question: "В книге или кинофильме меня больше всего привлекает:1",
+    },
+    {
+      question: "В книге или кинофильме меня больше всего привлекает:2",
+    },
+  ])
+
+  const setAnswer = (index: number, answer: Answer) => {
+    setData((prev) => {
+      const newArr = [...prev]
+      newArr[index].answer = answer
+      return newArr
+    })
+  }
 
   return (
     <div>
-      <div className={style.quizQustions} id="qustions">
-        <div className={style.quizQustionsItem}>
-          <div className={style.quizQustionsItemQustion}>{item.qustion}</div>
-          <ul className={style.quizQustionsItemAnswers}>
-            <li>
-              <label>
-                <input type="radio" name="q1" />
-                <div className={style.answer}>
-                  <p>{item.answer1}</p>
-                </div>
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="radio" name="q1" />
-                <div className={style.answer}>
-                  <p>{item.answer2}</p>
-                </div>
-              </label>
-            </li>
-          </ul>
+      {data.map((elem, index) => (
+        <div key={elem.question}>
+          <div className={style.qustion}>
+            <div className={style.qustionText}>
+              <p>{elem.question}</p>
+            </div>
+          </div>
+
+          <div>
+            <label>
+              <input
+                onClick={() => {
+                  setAnswer(index, Answer.Like)
+                }}
+                type="radio"
+                name={elem.question}
+              />
+              Нравится
+            </label>
+            <label>
+              <input
+                onClick={() => {
+                  setAnswer(index, Answer.DisLike)
+                }}
+                type="radio"
+                name={elem.question}
+              />
+              Не нравится
+            </label>
+            <label>
+              <input
+                onClick={() => {
+                  setAnswer(index, Answer.Idk)
+                }}
+                type="radio"
+                name={elem.question}
+              />
+              Затрудняюсь ответить
+            </label>
+          </div>
         </div>
-      </div>
-      {/* <div className={style.quizResults} id="results">
-        <div className={style.quizResultsItem}>
-          <div className={style.quizQustionsItemQustion}>{item.qustion}</div>
-          {answersRender}
-        </div>
-      </div> */}
+      ))}
+      <button
+        onClick={() => {
+          console.log(data)
+        }}
+      >
+        Завершить Тестирование
+      </button>
     </div>
-  );
-});
-const Quiz = () => {
-  return (
-    <div className={style.Quiz} id="quiz">
-      {/* <div className={style.quizIndicator}>1/10</div> */}
-      {DataContent}
-      <div className={style.quizControls}>
-        <button>Завершить тестирование</button>
-      </div>
-    </div>
-  );
-};
-export default Quiz;
+  )
+}
+export default Quiz
