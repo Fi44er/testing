8901a9dbe31b2c74be9feacbffc675c395e50819
Fi44er <vaceslavmalugin81@gmail.com>
2023-12-04 09:08:47 +0300

    add

--- testing/server/service/parserService.js
+++ testing/server/service/parserService.js
@@ -1,7 +1,13 @@
 const axios = require("axios")
+const words = require("../wordsGroups.json")
 
 class ParserService {
     async getGroups(userId) {
+
+
+        // console.log(words);
+
+
         const accessToken =
             "vk1.a.jtqHF1wPC9ZHJs2nQ9eN7oA4eOaA95BbIdU7-eySSB1ej1awZT4CNBblZGAv9-Pe7VhEEC6Nw-N5AQtSQkzbAPa6j8G9O1D_UKhtVnzSd2-MyFN4wXE1yjQhtXk112bJkBKB4DqReZ4sbLNfS9Z26Bzb9WjzcwO230yRWRYVR8iv6FfMjc0HM8-OWvOx_RJvTOBwbS_SZ6tycj7tMfSFIA" // ваш access_token
 
@@ -15,11 +21,17 @@ class ParserService {
                 responseDate = response.data
             })
         const groupIds = responseDate.response.groups.items
+
         await axios
-            .get(`https://api.vk.com/method/groups.getById?group_ids=${groupIds}&access_token=${accessToken}&v=5.131`)
-            .then((response) => {
-                nameGroups = response.data.response.map((group) => group.name)
-            })
+        .get(`https://api.vk.com/method/groups.getById?group_ids=35285306&fields=activity&access_token=${accessToken}&v=5.131`)
+        .then((response) => {
+            nameGroups = response.data
+        })
+        // await axios
+        //     .get(`https://api.vk.com/method/groups.getById?group_ids=${groupIds}&access_token=${accessToken}&v=5.131`)
+        //     .then((response) => {
+        //         nameGroups = response.data.response.map((group) => group.name)
+        //     })
         console.log(nameGroups);
     }
 }
